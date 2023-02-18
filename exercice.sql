21ae807656540279c16724dc6a6e391fd37d13b6
Michael Yapi .A <michaelakichi@gmail.com>
2023-02-18 12:37:08 +0000

    Update exercice.sql

--- exercice.sql
+++ exercice.sql
@@ -1,14 +1,12 @@
--- 1- Récupérez tous les films classés G ou PG, qui ne sont pas actuellement loués (ils ont été rendus/n'ont jamais été empruntés).
+-- Retrieve all films with a rating of G or PG, which are are not currently rented (they have been returned/have never been borrowed).
+
 SELECT *
 FROM movies
 LEFT JOIN rentals ON movies.movie_id = rentals.movie_id
 WHERE (rating = 'G' OR rating = 'PG') AND (rentals.rental_id IS NULL OR rentals.return_date IS NOT NULL)
 
+--Create a new table which will represent a waiting list for children’s movies. This will allow a child to add their name to the list until the DVD is available (has been returned). Once the child takes the DVD, their name should be removed from the waiting list (ideally using triggers, but we have not learned about them yet. Let’s assume that our Python program will manage this). Which table references should be included?
 
--- 2- Créez une nouvelle table qui représentera une liste d'attente pour les films pour enfants. Cela permettra à un enfant d'ajouter
--- son nom à la liste jusqu'à ce que le DVD soit disponible (a été retourné). Une fois que l'enfant prend le DVD, son nom doit être retiré 
--- de la liste d'attente (idéalement en utilisant des déclencheurs, mais nous ne les connaissons pas encore. Supposons que notre programme Python gère cela).
---Quelles références de table doivent être incluses ?
 CREATE TABLE waiting_list (
     waiting_id SERIAL PRIMARY KEY,
     movie_id INTEGER REFERENCES movies(movie_id),
@@ -17,7 +15,8 @@ CREATE TABLE waiting_list (
 );
 
 
--- 3- Récupérez le nombre de personnes qui attendent le DVD de chaque enfant. Testez cela en ajoutant des lignes au tableau que vous avez créé à la question 2 ci-dessus.
+-- Retrieve the number of people waiting for each children’s DVD. Test this by adding rows to the table that you created in question 2 above.
+
 SELECT movies.title, COUNT(waiting_list.waiting_id) AS num_waiting
 FROM movies
 LEFT JOIN waiting_list ON movies.movie_id = waiting_list.movie_id
